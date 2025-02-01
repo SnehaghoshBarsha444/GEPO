200e168ce7b7bdbc6455e2f1e0ec8574e19b38f8
Sneha Ghosh (Technical_Isopod_075) <153223798+SnehaghoshBarsha444@users.noreply.github.com>
2025-02-01 17:29:21 +0530

    Delete backend directory

--- backend/src/routes/fileRoutes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../controllers/FileController.php';
-
-use Controllers\FileController;
-
-
-$router->get("/image", [FileController::class, "getImage"]);
-$router->get("/video", [FileController::class, "getVideo"]);
--- backend/src/routes/studyAbroadEligibilityRoutes.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../controllers/studyAbroadEligibilityController.php';
-
-use Controllers\StudyAbroadEligibilityController;
-
-$router->get('/eligibility/queries', [StudyAbroadEligibilityController::class, 'getAllQueries']);
-$router->post('/eligibility/queries', [StudyAbroadEligibilityController::class, 'createQuery']);
-$router->delete('/eligibility/queries', [StudyAbroadEligibilityController::class, 'deleteQuery']);
